5eb5c97e3df97804d4736fabe2c4fdd422365943
navreet <navreet@improvonia.com>
2015-11-21 16:29:23 -0500

    comment changes

--- api/SocialCommunityDaoClasses/src/main/java/com/socialcommunity/domain/Comment.java
+++ api/SocialCommunityDaoClasses/src/main/java/com/socialcommunity/domain/Comment.java
@@ -4,15 +4,11 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "COMMENT")
+
 public class Comment implements Serializable {
 
 	private static final long serialVersionUID = -5527566248002296042L;
